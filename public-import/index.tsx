7e8e4e527ffba340367a0e2edf8edb5d3e3fa932
Pranay1278 <79448756+Pranay1278@users.noreply.github.com>
2022-06-16 13:14:08 +0530

    Add files via upload

---
+++ public-import/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+// import './web.css'
+
+
+
+
+function reuse(props:any){
+
+
+    return(
+       <div className="ui fluid card">
+           <div >
+               {props.header}
+           </div>
+           <div className="ui bottom attached button">
+               {props.footer}
+           </div>
+       </div>
+    )
+
+}
+
+export default reuse
